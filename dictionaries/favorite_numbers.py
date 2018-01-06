e9901df00683b03845375172f75184b0b49b9f94
Alexey Borodin <30794466+MrCodemaker@users.noreply.github.com>
2018-01-06 17:24:12 +0400

    dictionaries

---
+++ dictionaries/favorite_numbers.py
@@ -0,0 +1,16 @@
+favorite_numbers = {
+'eric': 2,
+'eliot': 1,
+'sarah': 23,
+'alice': 3,
+'james': 5,
+}
+print("Number " +
+    favorite_numbers['eric'] +
+    " is the favorite number")
+print("Number " +
+    favorite_numbers['eric'] +
+    " is the favorite number")
+print("Number " +
+        favorite_numbers['eric'] +
+        " is the favorite number")
---
+++ dictionaries/human.py
@@ -0,0 +1,16 @@
+human = {
+'first_name': 'daniel',
+'last_name': 'radcliffe',
+'age': 28,
+'city': 'london',
+}
+print("My name is " +
+    human['first_name'].title() +
+    human['last_name'].title() +
+    ".")
+print("I'm " +
+    str(human['age'] +
+    " years old.")
+print("I live in the " +
+    human['city'].title() +
+    " city.")   
