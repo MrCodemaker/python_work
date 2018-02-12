0b5366d3cea5eeb913f0e47d474327768b6103a3
Alexey Borodin <30794466+MrCodemaker@users.noreply.github.com>
2018-02-12 22:06:26 +0400

    function

--- function/greet_users.py
+++ function/greet_users.py
@@ -1,1 +1,8 @@
+def greet_users(names):
+    """Вывод простого приветствия в списке."""
+    for name in names:
+        msg = "Hello, " + name.title() + "!"
+        print(msg)
+usernames = ['hannah', 'ty', 'margot']
 
+greet_users(usernames)
---
+++ function/printing_models.py
@@ -0,0 +1,53 @@
+# Список моделей, которые необходимо напечатать.
+unprinted_designs =  ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+# Цикл последовательно печатает каждую модель до конца списка.
+# После печати каждая модель перемещается в список completed_models.
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    # Печать модель на 3D - принтере.
+    print("Printing model: " + current_design)
+    completed_models.append(current_design)
+
+# Вывод всех готовых моделей.
+print("\nThe following models have been printed:")
+for completed_model in completed_models:
+    print(completed_model)
+
+# С использованием функции.
+
+def print_models(unprinted_designs, completed_models):
+    """
+    Имитирует печать моделей, пока список не станет пустым.
+    Каждая модель после печати перемещается в completed_models.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+
+        # Имитация печати модели на 3D - принтере.
+        print("Printing model: " + current_design)
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    """Выводит информацию люл всех напечатанных моделях."""
+    print("\The following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
+
+"""
+Эта программа создает меньше проблем с расширением и сопровождением,
+чем версия без функций. Если позднее потребуется напечатать
+новую партию моделей, достаточно определить новый список unprinted_designs и
+снова вызвать print_models().
+Если окажется, что код печати необходимо модифицировать,
+изменения достаточно внести в одном месте, и они автоматически распространятся
+на все вызовы функции. Такой подход намного эффективнее независимой правки кода
+в нескольких местах программы.
+"""
